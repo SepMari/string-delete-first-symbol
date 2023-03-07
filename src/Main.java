5373de3819f27bf27d00013ca41e85da6eb8c193
lldv <48766898+SepMari@users.noreply.github.com>
2023-03-07 16:50:33 +0300

    Дан List <String> names. Удалите первую букву из каждого имени и верните отсортированный список.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = Arrays.asList("Java", "Python", "Ruby", "Kotlin");
+
+        List<String> modifiedNames = names.stream()
+                .map(name -> name.substring(1)) // удаляем первую букву из каждого имени
+                .sorted() // сортируем список
+                .collect(Collectors.toList()); // преобразовываем стрим к списку
+
+        System.out.println(modifiedNames); // [ava, otlin, uby, ython]
+
     }
+
 }
